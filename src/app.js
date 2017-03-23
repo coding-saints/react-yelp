07361a1c7e234a4acf3a170af2882fcc235fac4d
Nick Kaufmann <coding.saints2015@gmail.com>
2017-03-23 14:08:19 -0400

    add tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'font-awesome/css/font-awesome.css'
+import  './app.css';
+import styles from './styles.module.css'
 
 const App = React.createClass({
   render: function() {
-    return (<div>Text text text</div>)
+    return (
+      <div className={styles.wrapper}>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: </h1>
+      </div>
+    )
   }
 });
-
 const mountNode = document.querySelector('#root');
 ReactDOM.render(<App />, mountNode);
